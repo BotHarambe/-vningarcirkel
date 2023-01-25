929ac5f6750f090caea313a23d92ca6e0d62dd78
Samurai <kylett63@gmail.com>
2023-01-25 14:45:14 +0100

    Add project files.

---
+++ Övningarcirkel/Circle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övningarcirkel
+{
+    class Circle
+    {
+        private double radius = 1.0;
+
+        public Circle()
+        {
+        }
+
+        public Circle(double radius)
+        {
+            SetRadius(radius);
+        }
+
+        public double GetRadius()
+        {
+            return radius;
+        }
+
+        public void SetRadius(double radius)
+        {
+            if (radius > 0)
+            {
+                this.radius = radius;
+            }
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public double GetCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public override string ToString()
+        {
+            return "Radius: " + radius + ", Area: " + GetArea() + ", Circumference: " + GetCircumference();
+        }
+    }
+
+}
+
---
+++ Övningarcirkel/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övningarcirkel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många cirklar?");
+            int numCircles = int.Parse(Console.ReadLine());
+
+            var circles = new Circle[numCircles];
+
+            for (int i = 0; i < numCircles; i++)
+            {
+                Console.WriteLine("skriv radie av cirkeln " + (i + 1) + ":");
+                double radius = double.Parse(Console.ReadLine());
+
+                circles[i] = new Circle(radius);
+            }
+
+            foreach (Circle c in circles)
+            {
+                Console.WriteLine(c.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+
+
+    }
+
